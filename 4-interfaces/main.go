ace85d845b4f010ebeab9df2402c0bc2d51a8fa5
Drew Landis <landis.d@gmail.com>
2017-01-31 21:40:40 -0700

    simple program to calculate the occurrence of which date in November Thanksgiving falls on between year 0 and 2017

---
+++ 4-interfaces/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type probability struct {
+	twentytwo   int
+	twentythree int
+	twentyfour  int
+	twentyfive  int
+	twentysix   int
+	twentyseven int
+	twentyeight int
+}
+
+func main() {
+	p := probability{}
+	beginYear := 0
+	endYear := 2017
+
+	for i := beginYear; i <= endYear; i++ {
+		dateOfThanksgiving(i, &p)
+	}
+
+	prettyPrint(p, beginYear, endYear)
+}
+
+func prettyPrint(p probability, beginYear int, endYear int) {
+	fmt.Println()
+	space := ""
+
+	fmt.Println("Occurrence of Thanksgiving on each day between year ", beginYear, " and ", endYear)
+	fmt.Println("date:  22   23   24   25   26   27   28")
+	fmt.Println("occ:  ", p.twentytwo, space, p.twentythree, space, p.twentyfour, space, p.twentyfive, space, p.twentysix, space, p.twentyseven, space, p.twentyeight)
+}
+
+// Given a year, this function will return the day of the month that
+// Thanksgiving fell on for that year.  Thanksgiving falls on the 4th
+// Thursday of every November.  Thus, the response will be an int
+// somewhere between 22 and 28.
+func dateOfThanksgiving(year int, p *probability) {
+	novOneDay := time.Date(year, time.November, 1, 0, 0, 0, 0, time.UTC).Weekday().String()
+	switch novOneDay {
+	case "Sunday":
+		p.twentysix = p.twentytwo + 1
+		return
+	case "Monday":
+		p.twentyfive = p.twentytwo + 1
+		return
+	case "Tuesday":
+		p.twentyfour = p.twentytwo + 1
+		return
+	case "Wednesday":
+		p.twentythree = p.twentythree + 1
+		return
+	case "Thursday":
+		p.twentytwo = p.twentytwo + 1
+		return
+	case "Friday":
+		p.twentyeight = p.twentytwo + 1
+		return
+	case "Saturday":
+		p.twentyseven = p.twentytwo + 1
+		return
+	}
+	return
+}
